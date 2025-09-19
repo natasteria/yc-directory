e343439ed97f6272c8171831872a34b8e5a66e5d
natasteria <natasteria@users.noreply.github.com>
2025-09-19 19:14:55 +0300

    Use Live Content API for real-time startup post data

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -1,7 +1,7 @@
 import { defineQuery } from "next-sanity";
 
 export const STARTUPS_QUERY = 
-    defineQuery(`*[_type == "startup" && defined(slug.current)] {
+    defineQuery(`*[_type == "startup" && defined(slug.current)] | order(_createdAt desc){
     _id,
     title,
     _createdAt,
