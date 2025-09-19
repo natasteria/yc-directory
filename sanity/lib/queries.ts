5fe51be8a1bd8a0379c9a980ff8c3fbbdf54f343
natasteria <natasteria@users.noreply.github.com>
2025-09-19 23:44:16 +0300

    Setup PPR

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -14,3 +14,18 @@ export const STARTUPS_QUERY =
     category,
     image
     }`);
+
+export const STARTUPS_BY_ID_QUERY = defineQuery(`*[_type == "startup" && _id == $id][0] {
+  _id,
+  title,
+  _createdAt,
+  slug,
+  author -> {
+    _id, name, image, bio
+  },
+  views,
+  description,
+  category,
+  image,
+  pitch
+}`)
