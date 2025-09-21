f865675cb008c72da96e569787cd7044a3d703c3
natasteria <natasteria@users.noreply.github.com>
2025-09-21 23:22:59 +0300

    Implement real-time views count using sanity write client

--- components/View.tsx
+++ components/View.tsx
@@ -3,13 +3,18 @@ import Ping from './Ping'
 import { client } from '@/sanity/lib/client'
 import { STARTUPS_VIEWS_QUERY } from '@/sanity/lib/queries'
 import { formatViews } from '@/lib/utils'
+import { writeClient } from '@/sanity/lib/write-client'
+import { after } from 'next/server'
 
 const View = async({id}: {id: string}) => {
     const {views: totalViews} = await client
     .withConfig({useCdn: false})
     .fetch(STARTUPS_VIEWS_QUERY, {id});
 
-    // TODO: Update the number of views in real-time
+    after( async() => await writeClient
+        .patch(id)
+        .set({views: totalViews + 1})
+        .commit());
 
   return (
     <div className='view-container'>
