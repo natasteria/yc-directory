8a79e602391ded7621fdd5d376534e934ed97a82
natasteria <natasteria@users.noreply.github.com>
2025-09-24 12:39:42 +0300

    Implement Zod schema validation for StartupFrom

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,54 +1,55 @@
 import type { Metadata } from "next";
-import localFont from 'next/font/local'
+import localFont from "next/font/local";
 import "./globals.css";
-import 'easymde/dist/easymde.min.css';
+import "easymde/dist/easymde.min.css";
+import { Toaster } from "@/components/ui/toaster";
 
 const workSans = localFont({
   src: [
     {
-      path: './fonts/workSans-Black.ttf',
-      weight: '900',
-      style: 'normal'
+      path: "./fonts/workSans-Black.ttf",
+      weight: "900",
+      style: "normal",
     },
     {
-      path: './fonts/workSans-ExtraBold.ttf',
-      weight: '800',
-      style: 'normal'
+      path: "./fonts/workSans-ExtraBold.ttf",
+      weight: "800",
+      style: "normal",
     },
     {
-      path: './fonts/workSans-Bold.ttf',
-      weight: '700',
-      style: 'normal'
+      path: "./fonts/workSans-Bold.ttf",
+      weight: "700",
+      style: "normal",
     },
     {
-      path: './fonts/workSans-SemiBold.ttf',
-      weight: '600',
-      style: 'normal'
+      path: "./fonts/workSans-SemiBold.ttf",
+      weight: "600",
+      style: "normal",
     },
     {
-      path: './fonts/workSans-Medium.ttf',
-      weight: '500',
-      style: 'normal'
+      path: "./fonts/workSans-Medium.ttf",
+      weight: "500",
+      style: "normal",
     },
     {
-      path: './fonts/workSans-Regular.ttf',
-      weight: '400',
-      style: 'normal'
+      path: "./fonts/workSans-Regular.ttf",
+      weight: "400",
+      style: "normal",
     },
     {
-      path: './fonts/workSans-Thin.ttf',
-      weight: '200',
-      style: 'normal'
+      path: "./fonts/workSans-Thin.ttf",
+      weight: "200",
+      style: "normal",
     },
     {
-      path: './fonts/workSans-ExtraLight.ttf',
-      weight: '100',
-      style: 'normal'
+      path: "./fonts/workSans-ExtraLight.ttf",
+      weight: "100",
+      style: "normal",
     },
   ],
 
-  variable: '--font-work-sans',
-})
+  variable: "--font-work-sans",
+});
 
 export const metadata: Metadata = {
   title: "YC Directory",
@@ -62,10 +63,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${workSans.variable}`}
-      >
+      <body className={`${workSans.variable}`}>
         {children}
+        <Toaster />
       </body>
     </html>
   );
